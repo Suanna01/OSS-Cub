3546d29be817a09ed4a645605ce09a5091d3a6d7
Suanna01 <yangnony01@gamil.com>
2022-12-14 13:31:06 +0000

    #2 fix: mem_id 삭제

--- src/repositorys/hp.apply.repository.js
+++ src/repositorys/hp.apply.repository.js
@@ -136,7 +136,7 @@ class HpApplyModel {
     // 헬퍼 마이페이지) 개인정보 가져오기
     selectHpInfo = async (conn, mem_id) => {
         const selectHpIdcQuery =  `
-            SELECT mem_id, mem_gender, mem_birth
+            SELECT mem_gender, mem_birth
             FROM member
             WHERE mem_id = ?
         `;
